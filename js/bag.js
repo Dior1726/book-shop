2458ebc03d5be17e40102bae4fd270d5f9ea9c15
Dior1726 <muxammad_909@mail.ru>
2022-11-17 16:02:57 +0600

    fix: cart page bugs

--- js/bag.js
+++ js/bag.js
@@ -19,37 +19,6 @@ function createHeader() {
   logo.innerText = 'Book-Shop'
   welcomeText.innerText = 'Welcome to amazing book shop!'
   links.className = 'header-links'
-  cartLink.href = '/bag.html'
-  cartLink.className = 'link'
-  cartIcon.className = 'bx bx-cart'
-  storeLink.href = '/index.html'
-  storeLink.className = 'link'
-  storeIcon.className = 'bx bx-store-alt'
-  
-  storeLink.appendChild(storeIcon)
-  cartLink.appendChild(cartIcon)
-  links.appendChild(storeLink)
-  links.appendChild(cartLink)
-  container.appendChild(logo)
-  container.appendChild(welcomeText)
-  container.appendChild(links)
-  header.appendChild(container)
-  body.appendChild(header)
-}
-createHeader()
-
-function createHeader() {
-  const logo = document.createElement('h1')
-  const welcomeText = document.createElement('p')
-  const links = document.createElement('div')
-  const cartLink = document.createElement('a')
-  const cartIcon = document.createElement('i')
-  const storeLink = document.createElement('a')
-  const storeIcon = document.createElement('i')
-
-  logo.innerText = 'Book-Shop'
-  welcomeText.innerText = 'Welcome to amazing book shop!'
-  links.className = 'header-links'
   cartLink.href = './bag.html'
   cartLink.className = 'link'
   cartIcon.className = 'bx bx-cart'
@@ -107,7 +76,7 @@ function calculateTotal() {
     let sum = books.map((b) => b.price).reduce((prev, curr) => prev + curr)
     total.innerText = `Total: $${sum}`
   } else {
-    total.innerText = `Total: $0`
+    total.innerText = 'Total: $0'
   }
 }
 calculateTotal()
